129bb4a87b59b6d45784a10cc7ee8d79fdb2e6ad
Aina Gbolahan <ainagbolahan100@gmail.com>
2022-02-05 12:17:06 +0100

    added project

--- portfolio_website/lib/config/projects.dart
+++ portfolio_website/lib/config/projects.dart
@@ -6,4 +6,9 @@ final List<Project> projects = [
       image: 'assets/todo.jpg',
       description: 'Flutter ToDo App.',
       link: 'https://github.com/Ainagbolahan/flutter-todo-app'),
+  Project(
+      name: 'Portfolio Website',
+      image: 'assets/website.png',
+      description: 'Flutter Portfolio Website.',
+      link: 'https://github.com/Ainagbolahan/Flutter_portfolio_website'),
 ];
